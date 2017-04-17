8d725f324cd80efca6da4e6294588ba9673c5441
Matheus <matheus.britzke@ilegra.com>
2017-04-17 15:09:46 -0300

    classe Pessoa

---
+++ src/main/java/trabalhoFinal/Pessoa.java
@@ -0,0 +1,34 @@
+package trabalhoFinal;
+
+public class Pessoa {
+
+	private String nome;
+	private int idade;
+	
+	public Pessoa(String nome, int idade){
+		this.nome = nome;
+		this.idade = idade;
+	}
+	
+	public String getNome(){
+		return nome;
+	}
+	
+	public int idade(){
+		return idade;
+	}
+	
+	public void setNome(String novoNome){
+		nome = novoNome;
+	}
+	
+	public void setIdade(int novaIdade){
+		idade = novaIdade;
+	}
+	
+	@Override
+	public String toString(){
+		return "Nome: " + nome + "\nIdade: "+ idade;
+	}
+	
+}
