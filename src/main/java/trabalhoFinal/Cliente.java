4af19b2a9d5b47ecb776093dbabb5d45deba548c
Matheus <matheus.britzke@ilegra.com>
2017-04-20 08:09:06 -0300

    melhora da classe Cliente

--- src/main/java/trabalhoFinal/Cliente.java
+++ src/main/java/trabalhoFinal/Cliente.java
@@ -2,17 +2,43 @@ package trabalhoFinal;
 
 public class Cliente {
 
-	private Pessoa cliente;
+	private String nome;
+	private int idade;
 	private CPF cpfCliente;
-	
-	public Cliente(Pessoa novoCliente, CPF cpfNovoCliente){
-		cliente = novoCliente;
-		cpfCliente = cpfNovoCliente;
+
+	public Cliente(String nome, int idade, CPF cpfCliente) {
+		this.nome = nome;
+		this.idade = idade;
+		this.cpfCliente = cpfCliente;
 	}
-	
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public CPF getCpfCliente() {
+		return cpfCliente;
+	}
+
+	public void setCpfCliente(CPF cpfCliente) {
+		this.cpfCliente = cpfCliente;
+	}
+
 	@Override
-	public String toString(){
-		return "Cliente ; " + cliente.toString() + "\nCPF: " + cpfCliente.toString();
-	}
-	
+	public String toString() {
+		return "CLIENTE NOME =" + nome + "\nIDADE=" + idade + "\nCPF =" + cpfCliente + "\n";
+	}	
+
 }
