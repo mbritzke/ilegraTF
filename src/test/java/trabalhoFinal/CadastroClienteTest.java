295afc314a42eeeade0dd5b3c561ee739e0b28b2
Matheus <matheus.britzke@ilegra.com>
2017-04-25 12:50:34 -0300

    novos testes na Classe CadastroCliente

--- src/test/java/trabalhoFinal/CadastroClienteTest.java
+++ src/test/java/trabalhoFinal/CadastroClienteTest.java
@@ -1,7 +1,12 @@
 package trabalhoFinal;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import org.junit.Assert;
 import org.junit.Test;
+
+import junit.framework.AssertionFailedError;
 
 public class CadastroClienteTest {
 
@@ -23,4 +28,32 @@ public class CadastroClienteTest {
 		cadastroTeste.cadastro(cliente1);
 		Assert.assertFalse(cadastroTeste.cadastro(cliente2));
 	}
+	
+	@Test
+	public void testPesquisaCliente(){
+		CPF cpf1 = new CPF("19278172817");
+		CadastroCliente cadastroTeste = new CadastroCliente();
+		Cliente cliente1 = new Cliente("Nome", 100, cpf1);
+		cadastroTeste.cadastro(cliente1);
+		Assert.assertTrue(cadastroTeste.pesquisaCliente(cpf1));
+		CPF cpf2 = new CPF("127912617");
+		Assert.assertFalse(cadastroTeste.pesquisaCliente(cpf2));
+	}
+	
+	@Test
+	public void testRetornaLista(){
+		CPF cpf1 = new CPF("19278172817");
+		Cliente cliente1 = new Cliente("Nome1", 100, cpf1);
+		CPF cpf2 = new CPF("826816271");
+		Cliente cliente2 = new Cliente("Nome2", 45, cpf2);
+		CadastroCliente cadastroTeste = new CadastroCliente();
+		cadastroTeste.cadastro(cliente1);
+		cadastroTeste.cadastro(cliente2);
+		List<Cliente> ListaClientesAtual = new ArrayList<Cliente>();
+		ListaClientesAtual.add(cliente1);
+		ListaClientesAtual.add(cliente2);
+		List<Cliente> retornoLista = cadastroTeste.retornaLista();
+		Assert.assertEquals(retornoLista, retornoLista);
+	}
+	
 }
