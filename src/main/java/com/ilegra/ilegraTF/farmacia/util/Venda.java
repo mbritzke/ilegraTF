bee1b4e9e212f268f169025b71dd187583eeb350
Matheus Britzke <matheusbritzke@gmail.com>
2017-05-05 15:27:42 -0300

    Abstração da classe 'Vendas' para 'Venda' e teste

---
+++ src/main/java/com/ilegra/ilegraTF/farmacia/util/Venda.java
@@ -0,0 +1,20 @@
+package com.ilegra.ilegraTF.farmacia.util;
+
+import com.ilegra.ilegraTF.farmacia.Cliente;
+import com.ilegra.ilegraTF.farmacia.Medicamento;
+
+import java.util.List;
+
+public class Venda {
+
+    public double calculaVenda(Cadastro cadastroCliente, Cliente comprador, List<Medicamento> listaCompras){
+        boolean statusCliente = cadastroCliente.pesquisaCliente(comprador.getCpfCliente());
+        if(statusCliente == false)
+            return -1;
+        double valorCompra = 0;
+        for(Medicamento auxiliar: listaCompras){
+            valorCompra += auxiliar.getPreco();
+        }
+        return valorCompra;
+    }
+}
---
+++ src/test/java/com/ilegra/ilegraTF/farmacia/util/VendaTest.java
@@ -0,0 +1,31 @@
+package com.ilegra.ilegraTF.farmacia.util;
+
+import com.ilegra.ilegraTF.farmacia.Cliente;
+import com.ilegra.ilegraTF.farmacia.Medicamento;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class VendaTest {
+
+    Cadastro novoCadastro;
+    Venda vendaTeste;
+
+    @Before
+    public void inicializa(){
+        novoCadastro = new Cadastro();
+        vendaTeste = new Venda();
+    }
+
+    @Test
+    public void calculaVenda(){
+        Cliente clienteTeste = new Cliente("João", 18, new CPF("195.864.874-40"));
+        Medicamento medicamentoTeste = new Medicamento("Bepantol", "Derma Creme", "Dexpantenol", 20.0);
+        novoCadastro.cadastroCliente(clienteTeste);
+        novoCadastro.cadastraMedicamento(medicamentoTeste);
+        vendaTeste = new Venda();
+        double valor = vendaTeste.calculaVenda(novoCadastro, clienteTeste, novoCadastro.retornaListaMedicamentos());
+        Assert.assertEquals(20, (long)valor);
+    }
+}
